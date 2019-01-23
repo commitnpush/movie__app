8ad44b7875298f6ec626b3781bb13feab7ad327b
flynn <lognamu@gmail.com>
2019-01-23 19:36:35 +0900

    step 1 : view mock movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from "./Movie";
+
+const movies = [
+  {title:"Matrix" , poster:"https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Ultimate_Matrix_Collection_poster.jpg/220px-Ultimate_Matrix_Collection_poster.jpg"},
+  {title:"Full Metal Jacket" , poster:"https://images-na.ssl-images-amazon.com/images/I/81U3cu+0RAL._RI_.jpg"},
+  {title:"Old Boy" , poster:"https://affif-sitepublic-media-prod.s3.amazonaws.com/film_poster/0001/58/thumb_57624_film_poster_293x397.jpeg"},
+  {title:"Hunger Games" , poster:"https://prodimage.images-bn.com/pimages/9780439023528_p0_v1_s550x406.jpg"},
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {movies.map((v,i) => <Movie key={i} title={v.title} poster={v.poster}/>)}
       </div>
     );
   }
