f388ea505cdef50e3307869344bce6ce83fd0930
flynn <lognamu@gmail.com>
2019-01-24 15:38:33 +0900

    Updates

---
+++ precache-manifest.0bd030bbe98125c48ff59f80d870d8be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e6fd914dfa8ed5fe365",
+    "url": "/movie_app/static/css/main.cd87e036.chunk.css"
+  },
+  {
+    "revision": "3e6fd914dfa8ed5fe365",
+    "url": "/movie_app/static/js/main.3e6fd914.chunk.js"
+  },
+  {
+    "revision": "22fdec2062f68dfa3e16",
+    "url": "/movie_app/static/js/1.22fdec20.chunk.js"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "2803217415673767b222b213bbfbe0b9",
+    "url": "/movie_app/index.html"
+  }
+];
