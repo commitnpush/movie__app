e7c3016cb96317c52f8b0a67941f67c5934ff3dc
flynn <lognamu@gmail.com>
2019-01-24 16:10:35 +0900

    Updates

--- build/build/service-worker.js
+++ build/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.0bd030bbe98125c48ff59f80d870d8be.js"
+  "/precache-manifest.a0ab0d96f64d294761bf76ff5202e2da.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/movie_app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
